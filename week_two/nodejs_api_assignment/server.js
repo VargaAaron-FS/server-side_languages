5a258f6b4f7c2be396de6c7808ed57b8adac55b9
Aaron Varga <apvarga@student.fullsail.edu>
2022-03-09 15:40:35 -0500

    removed instructor led video code so that my own code only remains for instructor review/grading

--- week_two/nodejs_api_assignment/server.js
+++ week_two/nodejs_api_assignment/server.js
@@ -9,42 +9,6 @@ require("dotenv").config();
 
 const app = express();
 
-// Below is what I did in the follow-along video with you, the Instructor
-// // for localhost:3000/
-// app.get("/", (req, res, next) => {
-//   res.status(200).send("Service is UP!");
-// });
-
-// // get external service
-// // http://localhost:3000/todo
-// app.get("/todo", (req, res, next) => {
-//   todoService()
-//     .then((result) => res.status(200).json(result))
-//     .catch((err) =>
-//       res
-//         .status(501)
-//         .json({ error: { message: err.message, status: err.status } })
-//     );
-// });
-
-// // get external service by ID
-// // http://localhost:3000/todo/56
-// app.get("/todo/:todoId", (req, res, next) => {
-//   const todoId = req.params.todoId;
-//   todoServiceById(todoId)
-//     .then((result) => res.status(200).json(result))
-//     .catch((err) =>
-//       res.status(err.status || 501).json({
-//         error: {
-//           message: err.message,
-//           status: err.status,
-//           method: req.method,
-//         },
-//       })
-//     );
-// });
-
-// Below is my newly created dog photo API
 // for localhost:3000/
 app.get("/", (req, res, next) => {
   res.status(200).send("Service is UP!");
